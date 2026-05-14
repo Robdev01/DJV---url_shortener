ea24295a3950cd17a73d630e4a3436b8f2ce1c65
Robdev01 <robsondev01@outlook.com>
2026-05-14 12:09:13 -0300

    feat: adding updates to the service and controller

--- url-shortener/src/main/java/com/desafio/url_shortener/controller/ShortUrlController.java
+++ url-shortener/src/main/java/com/desafio/url_shortener/controller/ShortUrlController.java
@@ -1,4 +1,34 @@
 package com.desafio.url_shortener.controller;
 
+import com.desafio.url_shortener.dto.ShortenUrlRequest;
+import com.desafio.url_shortener.dto.ShortenUrlResponse;
+import com.desafio.url_shortener.service.ShortUrlService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
 public class ShortUrlController {
+
+    private final ShortUrlService service;
+
+    @PostMapping("/shorten-url")
+    public ResponseEntity<ShortenUrlResponse> shortenUrl(
+
+            @RequestBody
+            @Valid
+            ShortenUrlRequest request
+
+    ) {
+
+        ShortenUrlResponse response =
+                service.createShortUrl(request);
+
+        return ResponseEntity.ok(response);
+    }
 }
--- url-shortener/src/main/java/com/desafio/url_shortener/service/ShortUrlService.java
+++ url-shortener/src/main/java/com/desafio/url_shortener/service/ShortUrlService.java
@@ -1,4 +1,68 @@
 package com.desafio.url_shortener.service;
 
+import com.desafio.url_shortener.dto.ShortenUrlRequest;
+import com.desafio.url_shortener.dto.ShortenUrlResponse;
+import com.desafio.url_shortener.entity.ShortUrl;
+import com.desafio.url_shortener.repository.ShortUrlRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Random;
+
+@Service
+@RequiredArgsConstructor
 public class ShortUrlService {
+
+    private final ShortUrlRepository repository;
+
+    private static final String CHARS =
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    public ShortenUrlResponse createShortUrl(
+            ShortenUrlRequest request
+    ) {
+
+        String shortCode;
+
+        do {
+
+            shortCode = generateShortCode();
+
+        } while (repository.existsByShortCode(shortCode));
+
+        ShortUrl entity = ShortUrl.builder()
+                .originalUrl(request.url())
+                .shortCode(shortCode)
+                .createdAt(LocalDateTime.now())
+                .expiresAt(LocalDateTime.now().plusDays(30))
+                .build();
+
+        repository.save(entity);
+
+        return new ShortenUrlResponse(
+                "http://localhost:8080/" + shortCode
+        );
+    }
+
+    private String generateShortCode() {
+
+        Random random = new Random();
+
+        int size = 5;
+
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = 0; i < size; i++) {
+
+            int index =
+                    random.nextInt(CHARS.length());
+
+            builder.append(
+                    CHARS.charAt(index)
+            );
+        }
+
+        return builder.toString();
+    }
 }
